badc4d98ed1c7ce9b296784ed7a44f129afc5ca6
Dan McDonald <daniel.g.mcdonald@gmail.com>
2011-11-06 10:27:15 -0700

    Chat server opens up a PUB socket, sends test message every second

--- chatserver.py
+++ chatserver.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python
 
 import zmq
+from time import sleep
+
+def run_server():
+    ctx = zmq.Context()
+    pub = ctx.socket(zmq.PUB)
+    pub.bind('tcp://*:4455')
+
+    i = 0
+    while True:
+        pub.send("message %d" % i)
+        i += 1
+        sleep(1)
 
 if __name__ == '__main__':
-    print zmq.zmq_version()
+    run_server()
